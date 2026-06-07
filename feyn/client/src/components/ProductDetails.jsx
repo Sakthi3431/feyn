5d220cb4e9c80d3471acffe9d09dfd45b911af9c
Sakthi3431 <supersakthi0404@gmail.com>
2026-06-07 09:13:38 -0400

    bug fixes

--- feyn/client/src/components/ProductDetails.jsx
+++ feyn/client/src/components/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
-function ProductDetails({product, onAddCart}) {
+function ProductDetails({product}) {
     if (!product) {
     return <h2>Product not found</h2>;
   } 
